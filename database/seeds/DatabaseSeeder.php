b8e6def53d27e058970ef801510d4a6294c20973
frankmag <franklin.magoba@medbookafrica.com>
2020-05-08 15:31:42 +0300

    new seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,102 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(IncomingOrdersTableSeeder::class);
+        DB::table('pizza')->insert([
+            'name' => "Busia Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Classic Pizza with mozarella cheese and tomato sauce",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__7__oehiYXnV7Dk.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Nairobi Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Pizza served in a square bread, with peperonni and ham",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__8__-txOQbA2l6.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Mombasa Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Pizza with cherry tomatoes, peppers and onions",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__9__0bAXVH8aIb.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Kisumu Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Kisumu Style Pizza, with shredded mozarella cheese and tomatoe sauce",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__6__VzLRxyEdOPBw.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Eldoret Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Also known as the \"The Devil's Pizza\", because it was invented in the depths of hell. Why would you subject yourself to this?",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__3__IaCGV9G_l.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Kitale Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Kisumu style pizza, served on square bread used as a kind of deep plate",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__2__VXIC2t8bN.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Mandera Pizza",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "Mandera Style Pizza",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__4__Rw-M71JSv.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Margarita",
+            'price' => 10.5,
+            'type' => "pizza",
+            'description' => "The orginal, the one that started it all",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__1__wYMM0yMh2s.jpeg",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Large Soda",
+            'price' => 1.99,
+            'type' => "other",
+            'description' => "A large soda of your choice",
+            'image_url' => "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0022_Feed_Menu_0000_Drinks_Coca-cola.png",
+        ]);
+
+        DB::table('pizza')->insert([
+            'name' => "Medium Soda",
+            'price' => 1,
+            'type' => "other",
+            'description' => "A medium soda of your choice",
+            'image_url' => "https://ik.imagekit.io/714jicfzzb/images__5__Tapc-58GTDd.jpeg",
+        ]);
+
+        DB::table('currency')->insert([
+            'name' => "Dollar",
+            'symbol' => "$",
+            'exchange_rate' => 1,
+        ]);
+
+        DB::table('currency')->insert([
+            'name' => "Shillings",
+            'symbol' => "Ksh",
+            'exchange_rate' => 0.01,
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Test User",
+            'email' => 'test@testmail.com',
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
